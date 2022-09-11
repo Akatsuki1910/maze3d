012946661709a2f0a86fb284d9f673122b1c0207
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2022-09-11 12:02:42 +0900

    Initial Commit

---
+++ maze3d/maze3dView.h
@@ -0,0 +1,12 @@
+//
+//  maze3dView.h
+//  maze3d
+//
+//  Created by nakamura-ikuto on 2022/09/11.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface maze3dView : ScreenSaverView
+
+@end
---
+++ maze3d/maze3dView.m
@@ -0,0 +1,51 @@
+//
+//  maze3dView.m
+//  maze3d
+//
+//  Created by nakamura-ikuto on 2022/09/11.
+//
+
+#import "maze3dView.h"
+
+@implementation maze3dView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
